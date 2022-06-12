2eaffc88e2e9495c6ede959136bf6d2ed41a66b6
unknown <jchuks69@gmail.com>
2022-06-12 12:01:18 +0100

    fixed a bug on external load balancer security group

--- modules/Security/security.tf
+++ modules/Security/security.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "ext-alb-sg" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    description = "HTTP"
+    description = "HTTPS"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
